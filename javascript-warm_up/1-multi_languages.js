6d59c76553db26dbb48a4f96dd1961874f6a3cbd
Noha Hamdy <nohatota99@gmail.com>
2023-09-03 08:33:01 +0300

    1. 3 languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const C_language = 'C is fun';
+const python = 'Python is cool';
+const JS = 'JavaScript is amazing';
+console.log(C_language);
+console.log(python);
+console.log(JS);
