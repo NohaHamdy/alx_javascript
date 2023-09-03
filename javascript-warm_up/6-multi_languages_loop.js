cb6897483e57d5caa44efe15b4e179df879d010e
Noha Hamdy <nohatota99@gmail.com>
2023-09-03 13:06:27 +0300

    let's try again

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i=0; i < 3; i++)
-{
+for (let i = 0; i < 3; i++) {
     console.log(languages[i]);
 }
