608f377c201cb76ce0caf270e0423537b318d065
Noha Hamdy <nohatota99@gmail.com>
2023-09-04 06:31:04 +0300

    I don't know what is wrong

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++) {
     console.log(languages[i]);
