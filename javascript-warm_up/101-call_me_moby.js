f737826b1de4802c0aed91caeb6e636959ce74fd
Noha Hamdy <nohatota99@gmail.com>
2023-09-04 08:58:36 +0300

    changed the exporting method

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-function callMeMoby(x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) 
     theFunction();
 };
-module.exports { callMeMoby };
