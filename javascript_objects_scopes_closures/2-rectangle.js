b24cc5d78ab93dec5951093d8b270585e36ca50f
Noha Hamdy <nohatota99@gmail.com>
2023-09-07 12:29:17 +0300

    rearrange

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,8 @@
 class Rectangle {
     constructor (w, h) {
       if (typeof h === 'number' && h > 0 && typeof w === 'number' && w > 0) {
-        this.height = h;
         this.width = w;
+        this.height = h;
       }
     }
   }
