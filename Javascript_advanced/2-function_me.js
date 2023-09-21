3de02df37c821e625b6284742204acc30968cdb1
Noha Hamdy <nohatota99@gmail.com>
2023-09-21 11:44:27 +0300

    7. Stack order and setTimeout

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+function welcomeMessage(fullName) {
+    alert("Welcome " +fullName);
+}
+
+guillaume = welcomeMessage("Guillaume");
+alex = welcomeMessage("Alex");
+fred = welcomeMessage("Fred");
